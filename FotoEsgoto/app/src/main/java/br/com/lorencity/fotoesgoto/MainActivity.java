9004477510dbdea231ae8cc1c721a45740c1e752
Felipe Rastelli <felipe-rastelli@hotmail.com>
2015-09-15 00:54:53 -0300

    Começando a primeira tela

--- FotoEsgoto/app/src/main/java/br/com/lorencity/fotoesgoto/MainActivity.java
+++ FotoEsgoto/app/src/main/java/br/com/lorencity/fotoesgoto/MainActivity.java
@@ -1,16 +1,37 @@
 package br.com.lorencity.fotoesgoto;
 
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private EditText edtNome;
+    private Button btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edtNome = (EditText) findViewById(R.id.edtTextUser);
+        btnNext = (Button) findViewById(R.id.btnInicio);
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                dlg.setMessage(edtNome.getText().toString());
+                dlg.setNeutralButton("Ok", null);
+                dlg.show();
+            }
+        });
     }
 
     @Override
